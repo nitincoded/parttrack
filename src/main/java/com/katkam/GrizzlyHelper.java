f93eff8d3c923fd48dab68a866408301edd8a447
Nitin Reddy <nitincoded@gmail.com>
2017-02-12 20:49:23 +0400

    Database connection properties are no longer hardcoded - loaded from db.properties

--- src/main/java/com/katkam/GrizzlyHelper.java
+++ src/main/java/com/katkam/GrizzlyHelper.java
@@ -7,6 +7,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.util.Properties;
 
 /**
@@ -20,14 +23,60 @@ public class GrizzlyHelper {
     public SessionFactory getConcreteSessionFactory() throws Exception {
         SessionFactory concreteSessionFactory;
 
-        Class.forName("com.mysql.jdbc.Driver");
+        File f;
+
+        f = new File("db.properties"); //try current working directory
+        if (!f.exists()) f = new File(this.getClass().getResource("/").getPath() + "/db.properties"); //try root of the classpath
+
+        String dbDriver = null; //"com.mysql.jdbc.Driver";
+        String jdbcUrl = null; //"jdbc:mysql://localhost:3306/parttrack?serverTimezone=Asia/Dubai&user=root&password=pepsi";
+        String dbDialect = "org.hibernate.dialect.MySQLDialect";
+
+        if (f.exists() && !f.isDirectory()) {
+            InputStream is = new FileInputStream(f);
+            Properties dbProp = new Properties();
+
+            try {
+                dbProp.load(is);
+            }
+            finally {
+                is.close();
+            }
+
+            if (dbProp.containsKey("dbdriver")) {
+                dbDriver = dbProp.getProperty("dbdriver");
+                log.trace(String.format("DB Driver is: %s", dbDriver));
+            }
+
+            if (dbProp.containsKey("jdbcurl")) {
+                jdbcUrl = dbProp.getProperty("jdbcurl");
+                log.trace(String.format("JDBC URL is: %s", jdbcUrl));
+            }
+
+            if (dbProp.containsKey("dbdialect")) {
+                dbDialect = dbProp.getProperty("dbdialect");
+                log.trace(String.format("DB dialect is: %s", dbDialect));
+            } else {
+                log.trace(String.format("Reverting to default DB dialect: %s", dbDialect));
+            }
+        } else {
+            log.trace(f.getAbsolutePath() + " doesn't exist");
+        }
+
+        if (dbDriver != null) Class.forName(dbDriver);
 //            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
 
+
+        if (jdbcUrl == null) {
+            throw new Exception("JDBC URL not set");
+        }
+
+
         Properties prop = new Properties();
-        prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost:3306/parttrack?serverTimezone=Asia/Dubai&user=root&password=pepsi");
+        prop.setProperty("hibernate.connection.url", jdbcUrl);
 //        prop.setProperty("hibernate.connection.username", "root");
 //        prop.setProperty("hibernate.connection.password", "pepsi");
-        prop.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        prop.setProperty("dialect", dbDialect);
 
         prop.setProperty("hibernate.hbm2ddl.auto", "update");
 //            prop.setProperty("hibernate.hbm2ddl.auto", "create");
